35aaafa9986ff4b84424912472c78e3541147318
Alex <agordon1123@gmail.com>
2019-12-03 18:38:26 -0500

    removed pass from class LatLon

--- src/15_classes.py
+++ src/15_classes.py
@@ -5,7 +5,6 @@ class LatLon:
     def __init__(self, lat, lon):
         self.lat = lat
         self.lon = lon
-    pass
         
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
